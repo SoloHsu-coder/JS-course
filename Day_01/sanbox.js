8d051308cf91072d011e8c0e2b02e562b5ff2cca
SoloHsu-coder <solohsu321@gmail.com>
2022-06-02 06:51:45 +0630

    Day 01

---
+++ Day_01/sanbox.js
@@ -0,0 +1,20 @@
+// alert("hello");
+// console.log(1);
+// console.log(2);
+
+let age = 25;
+let year = 2022;
+
+console.log(age, year);
+
+year = 2015;
+console.log(year);
+
+const points = 100;
+//points = 150;
+console.log(points);
+
+/*var day = 1;
+console.log(day);
+day = 2;
+console.log(day);*/
