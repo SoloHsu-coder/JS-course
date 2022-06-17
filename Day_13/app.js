7269d043d45cb5cb0c2ab94b10d98eb4071526b2
SoloHsu-coder <solohsu321@gmail.com>
2022-06-17 07:55:44 +0630

    Day 14

--- Day_13/app.js
+++ Day_13/app.js
@@ -1,6 +1,6 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
-
+const result = document.querySelector(".result");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let score = 0;
@@ -18,5 +18,37 @@ form.addEventListener("submit", (e) => {
       score += 25;
     }
   });
-  console.log(score);
+  scrollTo(0, 0);
+
+  result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
+
+// window object (global object)
+
+// console.log(document.querySelector("form"));
+// window.console.log(document.querySelector("form"));
+// alert("hello");
+// window.alert("hello");
+
+// setTimeout(() => {
+//   alert("hello world");
+// }, 3000); // after 3 sec alert hello world
+// let i = 0;
+// const timer = setInterval(() => {
+//   alert("hello world");
+//   i++;
+//   if (i === 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000); // alert hello world every two second
